405ffd757261703b5acb9282645b3168a4da2042
yanhuaqiang <206936012@qq.com>
2018-12-07 14:56:20 +0800

    2

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -9,5 +9,6 @@ class ElectricMeter(http.Controller):
         elec_current = kw['elec_current']
         elec_voltage = kw['elec_voltage']
         elec_electricity = kw['elec_electricity']
-        request.env['electric.meter'].create({'elec_current': elec_current, 'elec_voltage': elec_voltage, 'elec_electricity': elec_electricity})
+        elec_str = kw['elec_str']
+        request.env['electric.meter'].create({'elec_current': elec_current, 'elec_voltage': elec_voltage, 'elec_electricity': elec_electricity, 'elec_str': elec_str})
         return "success"
--- models/models.py
+++ models/models.py
@@ -6,6 +6,11 @@ class electric_meter(models.Model):
     _name = 'electric.meter'
 
     name = fields.Char()
-    elec_current = fields.Integer(default=0, string='电流')
-    elec_voltage = fields.Integer(default=0, string='电压')
-    elec_electricity = fields.Integer(default=0, string='电量')
+    elec_id = fields.Integer(string='编号')
+    elec_current = fields.Float(default=0, string='电流')
+    elec_voltage = fields.Float(default=0, string='电压')
+    elec_electricity = fields.Float(default=0, string='电量')
+    elec_date = fields.Datetime()
+    elec_str = fields.Char(string='日期')
+
+
